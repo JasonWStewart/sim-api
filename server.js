c64c5aca5033829ab6a63f5319f775261406e1f9
Jason Stewart <jason@jasonstewart.io>
2023-01-26 14:38:56 +0000

    fix port allocation

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const port = 3000;
+const port = 8080;
 const iterations = +process.argv[process.argv.indexOf("-i") + 1] || 10000;
 const dateString = new Date().toISOString().slice(0, 10);
 
